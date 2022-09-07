c03c03235a3d906112f2110347b3561e65e17773
Hyun <hyeon998312@gmail.com>
2022-09-07 21:28:48 +0900

    rsi계산 구현

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from binance.client import Client
 import requests
 import pandas as pd
 from tqdm import tqdm
-
-
+import talib as ta
 
 with open("api.txt") as f:
     lines = f.readlines()
@@ -17,8 +16,6 @@ with open("api.txt") as f:
         'apiKey': api_key,
         'secret': api_secret
     })
-
-
     
 client = Client(api_key=api_key, api_secret=api_secret)
 
@@ -45,7 +42,6 @@ def cancelorder(symbol,orderId):
 
 def get_data(start_date, end_date, symbol, interv):
     data = []
-    
     start = int(time.mktime(datetime.strptime(start_date + ' 00:00', '%Y-%m-%d %H:%M').timetuple())) * 1000
     end = int(time.mktime(datetime.strptime(end_date +' 23:59', '%Y-%m-%d %H:%M').timetuple())) * 1000
     params = {
@@ -74,6 +70,39 @@ def get_data(start_date, end_date, symbol, interv):
     df['trades'] = df['trades'].astype(int)
     print(df[['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume',  'trades']])
 
+def buyalgorithm(symbol):
+    symbol = symbol
+    
+def future_account_info():
+    infos = client.futures_position_information(symbol='BTCUSDT')   #future position
+    df = pd.DataFrame(infos)
+    df = df.drop(columns=['markPrice'])
+    df = df.drop(columns=['isAutoAddMargin'])
+    df = df.drop(columns=['maxNotionalValue'])
+    df = df.drop(columns=['notional'])
+    df = df.drop(columns=['isolatedMargin'])
+    df = df.drop(columns=['isolatedWallet'])
+    df = df.drop(columns=['updateTime'])
+    print(df)
+
+def setcandledata(symbol):
+    from datetime import datetime, timezone
+    from binance.spot import Spot as cl
+    client = cl(api_key,api_secret)
+    symbol = symbol #BTCUSDT로 현재 fix
+    klines = client.klines('BTCUSDT','4h',limit=200) #캔들 원하는 봉 갯수
+    df = pd.DataFrame(data={
+        'open_time' : [datetime.fromtimestamp(x[0]/1000, timezone.utc) for x in klines],
+        'open' : [float(x[1]) for x in klines],
+        'high' : [float(x[2]) for x in klines],
+        'low' : [float(x[3]) for x in klines],
+        'close' : [float(x[4]) for x in klines],
+        'volume' : [float(x[5]) for x in klines],
+        'close_time' : [datetime.fromtimestamp(x[6]/1000,timezone.utc) for x in klines],
+    })
+    ta_rsi = ta.RSI(df['close'],timeperiod=14)
+    print(ta_rsi[-1:].values[0])
+
 
 info = client.get_account()
 df = pd.DataFrame(info["balances"])
@@ -83,7 +112,7 @@ print(df)
 
 portfolio = ["BTCUSDT","ETHUSDT","XRPUSDT"]
 while True:
-    signal = int(input("1 : see price\n2 : buy order\n3 : sell order\n4 : see past price\n"))
+    signal = int(input("1 : see price\n2 : buy order\n3 : sell order\n4 : see past price\n5 : see future account\n6 : see coin's rsi\n"))
     if(signal==1):
         while True:
             coin = client.get_symbol_ticker(symbol=portfolio[2])
@@ -126,6 +155,12 @@ while True:
         end_date = input("put in end date : ")  #set : 2022-08-01
         interv = input("put in interval : ")
         get_data(start_date, end_date, sym, interv)
+    elif(signal==5):
+        future_account_info()
+    elif(signal==6):
+        setcandledata('BTCUSDT') #rsi가 20~30에서 매수, 70~80에선 매도를 하도록 구현할것
+        
+        
                 
 
 
